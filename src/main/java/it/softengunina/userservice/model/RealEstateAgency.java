a7611916fa21f57a4ecfc35b5ea3c2f66923319b
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-24 11:44:44 +0200

    removed onetomany relations

--- src/main/java/it/softengunina/userservice/model/RealEstateAgency.java
+++ src/main/java/it/softengunina/userservice/model/RealEstateAgency.java
@@ -1,13 +1,8 @@
 package it.softengunina.userservice.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
-
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "real_estate_agencies")
@@ -32,49 +27,10 @@ public class RealEstateAgency{
     @Setter
     private String name;
 
-    @OneToMany(mappedBy = "agency")
-    @JsonManagedReference
-    @Setter(AccessLevel.PROTECTED)
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    private Set<RealEstateManager> managers = new HashSet<>();
-
-    @OneToMany(mappedBy = "agency")
-    @JsonManagedReference
-    @Setter(AccessLevel.PROTECTED)
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    private Set<RealEstateAgent> agents = new HashSet<>();
-
     public RealEstateAgency(@NonNull String iban,
                             @NonNull String name) {
         this.iban = iban;
         this.name = name;
-    }
-
-    public Set<RealEstateManager> getManagers() {
-        return Collections.unmodifiableSet(managers);
-    }
-
-    public Set<RealEstateAgent> getAgents() {
-        return Collections.unmodifiableSet(agents);
-    }
-
-    public void addManager(@NonNull RealEstateManager manager) {
-        this.agents.add(manager);
-        this.managers.add(manager);
-    }
-
-    public void addAgent(@NonNull RealEstateAgent agent) {
-        this.agents.add(agent);
-    }
-
-    public void removeManager(RealEstateManager manager) {
-        this.managers.remove(manager);
-    }
-
-    public void removeAgent(RealEstateAgent agent) {
-        this.agents.remove(agent);
     }
 }
 
