69eafa29a72ab34d029385a3e1d84a3349367671
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-25 13:57:19 +0200

    added PromotionServiceTest

--- src/test/java/it/softengunina/userservice/services/PromotionServiceTest.java
+++ src/test/java/it/softengunina/userservice/services/PromotionServiceTest.java
@@ -6,6 +6,7 @@ import it.softengunina.userservice.model.RealEstateManager;
 import it.softengunina.userservice.model.User;
 import it.softengunina.userservice.repository.RealEstateAgencyRepository;
 import it.softengunina.userservice.repository.RealEstateAgentRepository;
+import it.softengunina.userservice.repository.RealEstateManagerRepository;
 import it.softengunina.userservice.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,14 +31,82 @@ class PromotionServiceTest {
     PromotionService promotionService;
 
     RealEstateAgency agency;
+    RealEstateManager manager;
     RealEstateAgent agent;
     User user;
+    @Autowired
+    private RealEstateManagerRepository realEstateManagerRepository;
 
     @BeforeEach
     void setUp() {
         agency = agencyRepository.saveAndFlush(new RealEstateAgency("agencyIban", "agencyName"));
+        manager = realEstateManagerRepository.saveAndFlush(new RealEstateManager("managerUsername", "managerSub", agency));
         agent = agentRepository.saveAndFlush(new RealEstateAgent("agentUsername", "agentSub", agency));
         user = userRepository.saveAndFlush(new User("userUsername", "userSub"));
+    }
+
+    @Test
+    void promoteToAgent() {
+        RealEstateAgent promotedAgent = promotionService.promoteToAgent(user, agency);
+        assertAll(
+            () -> assertNotNull(promotedAgent),
+            () -> assertEquals(user.getId(), promotedAgent.getId()),
+            () -> assertEquals(agency.getId(), promotedAgent.getAgency().getId())
+        );
+    }
+
+    @Test
+    void promoteToAgent_UserIsAgent() {
+        assertThrows(IllegalArgumentException.class, () ->
+                promotionService.promoteToAgent(agent, agency)
+        );
+    }
+
+    @Test
+    void promoteToManager() {
+        RealEstateManager promotedManager = promotionService.promoteToManager(user, agency);
+        assertAll(
+                () -> assertNotNull(promotedManager),
+                () -> assertEquals(user.getId(), promotedManager.getId()),
+                () -> assertEquals(agency.getId(), promotedManager.getAgency().getId())
+        );
+    }
+
+    @Test
+    void promoteToManager_UserIsAgentFromSameAgency() {
+        RealEstateManager promotedManager = promotionService.promoteToManager(agent, agency);
+        assertAll(
+                () -> assertNotNull(promotedManager),
+                () -> assertEquals(agent.getId(), promotedManager.getId()),
+                () -> assertEquals(agency.getId(), promotedManager.getAgency().getId())
+        );
+    }
+
+    @Test
+    void promoteToManager_UserIsAgentFromAnotherAgency() {
+        RealEstateAgency anotherAgency = agencyRepository.saveAndFlush(new RealEstateAgency("anotherIban", "anotherName"));
+        RealEstateAgent anotherAgent = agentRepository.saveAndFlush(new RealEstateAgent("anotherUsername", "anotherSub", anotherAgency));
+
+        assertThrows(IllegalArgumentException.class, () ->
+                promotionService.promoteToManager(anotherAgent, agency)
+        );
+    }
+
+    @Test
+    void promoteToManager_UserIsManagerFromSameAgency() {
+        assertThrows(IllegalArgumentException.class, () ->
+            promotionService.promoteToManager(manager, agency)
+        );
+    }
+
+    @Test
+    void promoteToManager_UserIsManagerFromAnotherAgency() {
+        RealEstateAgency anotherAgency = agencyRepository.saveAndFlush(new RealEstateAgency("anotherIban", "anotherName"));
+        RealEstateManager anotherManager = realEstateManagerRepository.saveAndFlush(new RealEstateManager("anotherManagerUsername", "anotherManagerSub", anotherAgency));
+
+        assertThrows(IllegalArgumentException.class, () ->
+            promotionService.promoteToManager(anotherManager, agency)
+        );
     }
 
     @Test
@@ -68,4 +137,5 @@ class PromotionServiceTest {
                 () -> assertEquals(agency.getId(), promotedManager.getAgency().getId())
         );
     }
+
 }
