b100afa16be5e5e2697105b763f6042bf70a0bcf
WaLoigg <167706159+WaLoigg@users.noreply.github.com>
2025-08-25 20:01:43 +0200

    added Insertion model repository and controller

---
+++ src/main/java/it/softengunina/userservice/model/insertions/InsertionForRent.java
@@ -0,0 +1,21 @@
+package it.softengunina.userservice.model.insertions;
+
+import it.softengunina.userservice.model.RealEstateAgent;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class InsertionForRent extends Insertion {
+    private double rent;
+
+    public InsertionForRent(Address address, InsertionDetails details, RealEstateAgent uploader, double rent) {
+        super(address, details, uploader);
+        this.rent = rent;
+    }
+}
