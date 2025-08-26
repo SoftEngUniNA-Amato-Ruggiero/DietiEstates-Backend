e4bfcdbc62d4680b163563d5a9b6cb0fb8473853
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-26 09:55:58 +0200

    added getter and setter for price and rent

--- src/main/java/it/softengunina/dietiestatesbackend/model/insertions/InsertionForRent.java
+++ src/main/java/it/softengunina/dietiestatesbackend/model/insertions/InsertionForRent.java
@@ -2,16 +2,15 @@ package it.softengunina.dietiestatesbackend.model.insertions;
 
 import it.softengunina.dietiestatesbackend.model.users.RealEstateAgent;
 import jakarta.persistence.Entity;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class InsertionForRent extends Insertion {
+    @Getter
+    @Setter
     private double rent;
 
     public InsertionForRent(Address address, InsertionDetails details, RealEstateAgent uploader, double rent) {
