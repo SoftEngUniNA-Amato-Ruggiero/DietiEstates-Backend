01a17efddefb46295de737fd9a6567512a3fa534
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-30 21:44:41 +0200

    tests for UserNotAffiliatedWithAgencyServiceTest

---
+++ src/test/java/it/softengunina/dietiestatesbackend/services/UserNotAffiliatedWithAgencyServiceTest.java
@@ -0,0 +1,82 @@
+package it.softengunina.dietiestatesbackend.services;
+
+import it.softengunina.dietiestatesbackend.exceptions.UserIsAlreadyAffiliatedWithAgencyException;
+import it.softengunina.dietiestatesbackend.model.RealEstateAgency;
+import it.softengunina.dietiestatesbackend.model.users.BaseUser;
+import it.softengunina.dietiestatesbackend.model.users.RealEstateAgent;
+import it.softengunina.dietiestatesbackend.repository.usersrepository.BaseUserRepository;
+import it.softengunina.dietiestatesbackend.repository.usersrepository.RealEstateAgentRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserNotAffiliatedWithAgencyServiceTest {
+    @Mock
+    BaseUserRepository userRepository;
+    @Mock
+    RealEstateAgentRepository agentRepository;
+
+    AutoCloseable mocks;
+
+    UserNotAffiliatedWithAgencyService service;
+    BaseUser user;
+    RealEstateAgent agent;
+    RealEstateAgency agency;
+
+    @BeforeEach
+    void setUp() {
+        mocks = MockitoAnnotations.openMocks(this);
+        service = new UserNotAffiliatedWithAgencyService(userRepository, agentRepository);
+        user = new BaseUser("baseUserName", "baseUserCognitoSub");
+        agency = new RealEstateAgency("agencyIban", "agencyName");
+        agent = new RealEstateAgent(new BaseUser("agentUserName", "agentUserCognitoSub"), agency);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        mocks.close();
+    }
+
+    @Test
+    void findByCognitoSub() {
+        Mockito.when(agentRepository.findByUser_CognitoSub(user.getCognitoSub())).thenReturn(Optional.empty());
+        Mockito.when(userRepository.findByCognitoSub(user.getCognitoSub())).thenReturn(Optional.of(user));
+
+        Optional<BaseUser> foundUser = service.findByCognitoSub(user.getCognitoSub());
+        assertTrue(foundUser.isPresent());
+    }
+
+    @Test
+    void findByCognitoSub_WhenAgent() {
+        Mockito.when(agentRepository.findByUser_CognitoSub(agent.getCognitoSub())).thenReturn(Optional.of(agent));
+        Mockito.when(userRepository.findByCognitoSub(agent.getCognitoSub())).thenReturn(Optional.of(agent.getUser()));
+
+        String cognitoSub = agent.getCognitoSub();
+        assertThrows(UserIsAlreadyAffiliatedWithAgencyException.class, () -> service.findByCognitoSub(cognitoSub));
+    }
+
+    @Test
+    void findByUsername() {
+        Mockito.when(agentRepository.findByUser_Username(user.getUsername())).thenReturn(Optional.empty());
+        Mockito.when(userRepository.findByUsername(user.getCognitoSub())).thenReturn(Optional.of(user));
+
+        Optional<BaseUser> foundUser = service.findByUsername(user.getCognitoSub());
+        assertTrue(foundUser.isPresent());
+    }
+
+    @Test
+    void findByUsername_WhenAgent() {
+        Mockito.when(agentRepository.findByUser_Username(agent.getUsername())).thenReturn(Optional.of(agent));
+        Mockito.when(userRepository.findByUsername(agent.getUsername())).thenReturn(Optional.of(agent.getUser()));
+
+        String username = agent.getUsername();
+        assertThrows(UserIsAlreadyAffiliatedWithAgencyException.class, () -> service.findByUsername(username));
+    }
+}
