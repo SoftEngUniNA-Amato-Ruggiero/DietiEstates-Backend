b1f9a91e613a06cc616db6ec61c14ef3031f0052
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-31 03:05:04 +0200

    allowed unauthenticated get requests for insertions

--- src/main/java/it/softengunina/dietiestatesbackend/SecurityConfiguration.java
+++ src/main/java/it/softengunina/dietiestatesbackend/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package it.softengunina.dietiestatesbackend;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,6 +38,7 @@ public class SecurityConfiguration {
             )
             .authorizeHttpRequests(authz -> authz
                     .requestMatchers(h2ConsolePath+"/**", apiDocsPath+"/**", swaggerPath+"/**").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/insertions", "/insertions/**").permitAll()
                     .anyRequest().authenticated()
             )
             .sessionManagement(session -> session
