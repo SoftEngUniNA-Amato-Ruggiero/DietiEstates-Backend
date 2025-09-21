dea83d5aefa5abed800886f5bfdcb5bd0d141c1e
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-09-21 15:59:18 +0200

    added indexes for non unique fields

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -122,3 +122,12 @@
        foreign key (user_id) 
        references business_users 
        on delete cascade;
+
+    create index if not exists addresses_location on addresses using gist(location);
+    create index if not exists insertions_uploader_id on insertions(uploader_id);
+    create index if not exists insertions_agency_id on insertions(agency_id);
+    create index if not exists insertions_address_id on insertions(address_id);
+    create index if not exists insertions_tags on insertion_tags(tags);
+    create index if not exists insertions_for_rent_rent on insertions_for_rent(rent);
+    create index if not exists insertions_for_sale_price on insertions_for_sale(price);
+    create index if not exists business_users_agency_id on business_users(agency_id);
