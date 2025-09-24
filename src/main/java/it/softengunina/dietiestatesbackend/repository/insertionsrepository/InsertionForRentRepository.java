785dd6173c343e8963b744278a9daa98467754dc
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-09-24 15:02:57 +0200

    added some methods for insertion repositories

---
+++ src/main/java/it/softengunina/dietiestatesbackend/repository/insertionsrepository/InsertionForRentRepository.java
@@ -0,0 +1,11 @@
+package it.softengunina.dietiestatesbackend.repository.insertionsrepository;
+
+import it.softengunina.dietiestatesbackend.model.insertions.InsertionForRent;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface InsertionForRentRepository extends InsertionRepository<InsertionForRent> {
+    Page<InsertionForRent> findByRent(Double rent, Pageable pageable);
+    Page<InsertionForRent> findByRentLessThanEqual(Double rent, Pageable pageable);
+    Page<InsertionForRent> findByRentGreaterThanEqual(Double rent, Pageable pageable);
+}
