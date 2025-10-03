da641b3d25a9222256349b115a606c2519a48b65
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-10-03 13:12:41 +0200

    added AgencyControllerIntegrationTest

---
+++ src/test/java/it/softengunina/dietiestatesbackend/controller/AgencyControllerIntegrationTest.java
@@ -0,0 +1,48 @@
+package it.softengunina.dietiestatesbackend.controller;
+
+import it.softengunina.dietiestatesbackend.dto.RealEstateAgencyRequestDTO;
+import it.softengunina.dietiestatesbackend.dto.usersdto.BusinessUserResponseDTO;
+import it.softengunina.dietiestatesbackend.dto.usersdto.UserResponseDTO;
+import it.softengunina.dietiestatesbackend.services.TokenService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class AgencyControllerIntegrationTest {
+    @Autowired
+    AgencyController agencyController;
+
+    @MockitoBean
+    TokenService tokenService;
+
+    @Test
+    void getAgentsByAgencyId() {
+        Page<UserResponseDTO> res = agencyController.getAgentsByAgencyId(10L, null);
+
+        assertNotNull(res);
+        assertTrue(res.hasContent());
+        assertEquals(1, res.getTotalElements());
+    }
+
+    @Test
+    void createAgency() {
+        Mockito.when(tokenService.getCognitoSub()).thenReturn("baseUserSub");
+        RealEstateAgencyRequestDTO req = new RealEstateAgencyRequestDTO("1234567890", "New Agency");
+
+        BusinessUserResponseDTO res = agencyController.createAgency(req);
+
+        assertNotNull(res);
+        assertEquals("New Agency", res.getAgency().getName());
+        assertEquals("1234567890", res.getAgency().getIban());
+    }
+}
