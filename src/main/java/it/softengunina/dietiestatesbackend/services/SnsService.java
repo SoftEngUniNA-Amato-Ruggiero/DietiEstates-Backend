0d5c8aa84c94496bbe592814dd3be3fcc9261f2d
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-10-19 21:07:54 +0200

    using try with resources

--- src/main/java/it/softengunina/dietiestatesbackend/services/SnsService.java
+++ src/main/java/it/softengunina/dietiestatesbackend/services/SnsService.java
@@ -17,77 +17,55 @@ public class SnsService {
     private static final ProfileCredentialsProvider CREDENTIALS_PROVIDER = ProfileCredentialsProvider.create();
     
     public void withClient(Consumer<SnsClient> consumer) {
-        SnsClient snsClient = SnsClient.builder()
+        try (SnsClient snsClient = SnsClient.builder()
                 .region(Region.EU_WEST_3)
                 .credentialsProvider(CREDENTIALS_PROVIDER)
-                .build();
-
-        consumer.accept( snsClient );
-
-        snsClient.close();
+                .build()
+        ) {
+            consumer.accept(snsClient);
+        } catch (SnsException e) {
+            log.error(e.awsErrorDetails().errorMessage());
+        }
     }
     
     public void listTopics(SnsClient snsClient) {
-        try {
-            ListTopicsIterable listTopics = snsClient.listTopicsPaginator();
-            listTopics.stream()
-                    .flatMap(r -> r.topics().stream())
-                    .forEach(content -> log.info(" Topic ARN: {}", content.topicArn()));
-
-        } catch (SnsException e) {
-            log.error(e.awsErrorDetails().errorMessage());
-        }
+        ListTopicsIterable listTopics = snsClient.listTopicsPaginator();
+        listTopics.stream()
+                .flatMap(r -> r.topics().stream())
+                .forEach(content -> log.info(" Topic ARN: {}", content.topicArn()));
     }
     
     public PublishResponse pubTopic(SnsClient snsClient, String message) {
-        try {
-            PublishRequest request = PublishRequest.builder()
-                    .message(message)
-                    .topicArn(SnsService.TOPIC_ARN)
-                    .build();
+        PublishRequest request = PublishRequest.builder()
+                .message(message)
+                .topicArn(SnsService.TOPIC_ARN)
+                .build();
 
-            PublishResponse result = snsClient.publish(request);
-            log.info("{} Message sent. Status is {}", result.messageId(), result.sdkHttpResponse().statusCode());
-            return result;
-
-        } catch (SnsException e) {
-            log.error(e.awsErrorDetails().errorMessage());
-            throw e;
-        }
+        PublishResponse result = snsClient.publish(request);
+        log.info("{} Message sent. Status is {}", result.messageId(), result.sdkHttpResponse().statusCode());
+        return result;
     }
 
     public SubscribeResponse subEmail(SnsClient snsClient, String email) {
-        try {
-            SubscribeRequest request = SubscribeRequest.builder()
-                    .protocol("email")
-                    .endpoint(email)
-                    .returnSubscriptionArn(true)
-                    .topicArn(SnsService.TOPIC_ARN)
-                    .build();
+        SubscribeRequest request = SubscribeRequest.builder()
+                .protocol("email")
+                .endpoint(email)
+                .returnSubscriptionArn(true)
+                .topicArn(SnsService.TOPIC_ARN)
+                .build();
 
-            SubscribeResponse result = snsClient.subscribe(request);
-            log.info("Subscription ARN: {}\n\n Status is {}", result.subscriptionArn(), result.sdkHttpResponse().statusCode());
-            return result;
-
-        } catch (SnsException e) {
-            log.error(e.awsErrorDetails().errorMessage());
-            throw e;
-        }
+        SubscribeResponse result = snsClient.subscribe(request);
+        log.info("Subscription ARN: {}\n\n Status is {}", result.subscriptionArn(), result.sdkHttpResponse().statusCode());
+        return result;
     }
     
     public UnsubscribeResponse unsubEmail(SnsClient snsClient, String subscriptionArn) {
-        try {
-            UnsubscribeRequest request = UnsubscribeRequest.builder()
-                    .subscriptionArn(subscriptionArn)
-                    .build();
+        UnsubscribeRequest request = UnsubscribeRequest.builder()
+                .subscriptionArn(subscriptionArn)
+                .build();
 
-            UnsubscribeResponse result = snsClient.unsubscribe(request);
-            log.info("Unsubscribed: {}", subscriptionArn);
-            return result;
-
-        } catch (SnsException e) {
-            log.error(e.awsErrorDetails().errorMessage());
-            throw e;
-        }
+        UnsubscribeResponse result = snsClient.unsubscribe(request);
+        log.info("Unsubscribed: {}", subscriptionArn);
+        return result;
     }
 }
