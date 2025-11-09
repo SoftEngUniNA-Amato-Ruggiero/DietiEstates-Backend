15233dc9f6b8c0d1b6e3218eb704f9b9c571a52e
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-11-09 08:48:56 +0100

    renamed NotificationsPreferencesDTO and added constructor

---
+++ src/main/java/it/softengunina/dietiestatesbackend/dto/NotificationsPreferencesDTO.java
@@ -0,0 +1,19 @@
+package it.softengunina.dietiestatesbackend.dto;
+
+import it.softengunina.dietiestatesbackend.model.NotificationsPreferences;
+import lombok.Data;
+
+@Data
+public class NotificationsPreferencesDTO {
+    private String city;
+    private Boolean emailNotificationsEnabled;
+    private Boolean notificationsForSaleEnabled;
+    private Boolean notificationsForRentEnabled;
+
+    public NotificationsPreferencesDTO(NotificationsPreferences prefs) {
+        this.city = prefs.getCity();
+        this.emailNotificationsEnabled = prefs.isEmailNotificationsEnabled();
+        this.notificationsForSaleEnabled = prefs.isNotificationsForSaleEnabled();
+        this.notificationsForRentEnabled = prefs.isNotificationsForRentEnabled();
+    }
+}
